ddc5e41eccbaebb2e924343d84f5df99b5225e43
AlexRashidov <[rashidov.sasha@yandex.ru]>
2023-07-20 17:51:21 +0700

    Pt_3_5:Full Task

---
+++ Practice3_5.py
@@ -0,0 +1,24 @@
+def isPrime(x): #Функция, чтобы определить простоту числа
+    dl = []
+    ln = 0
+    for i in range(1,x + 1):
+        if (x % i) == 0:
+            dl.append(i)
+    if len(dl) == 2: #если длина списка делителей числа == 2, то число простое, если  останется 0, то число составное
+        ln = len(dl)
+    return ln
+
+
+a = 1
+b = 100
+notPrime = []
+for num in range(a, b + 1):
+    c = isPrime(num)
+    if c == 0: #Проверил, чтобы число было составным
+        notPrime.append(num)
+print(notPrime)
+
+
+
+
+
