79d780784e5057c9b61b483c227fea2e9efca813
AlexRashidov <[rashidov.sasha@yandex.ru]>
2023-07-20 17:19:14 +0700

    Pt_1_4:Full Task

---
+++ Practice4.py
@@ -0,0 +1,12 @@
+num = int(input("Введите число от 10 до 20\n"))
+flag = False
+while flag == False:
+    if num < 10:
+        print("Введенное числов меньше 10")
+        num = int(input("Введите число от 10 до 20\n"))
+    elif (num > 20):
+        print("Введенное числов больше 20")
+        num = int(input("Введите число от 10 до 20\n"))
+    else:
+        flag = True
+        print(num, "Спасибо")
