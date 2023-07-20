a2f550d11a5353652086d822eb6b24805e379a43
AlexRashidov <[rashidov.sasha@yandex.ru]>
2023-07-20 20:33:56 +0700

    Pt_4_4:Full Task

---
+++ Practice4_4.py
@@ -0,0 +1,12 @@
+import itertools, random
+nums = [random.randint(0, 10 + 1) for i in range(10 + 1)]
+combinations = []
+a = 10
+for r in range(1, len(nums) + 1):
+    comb = list(itertools.combinations(nums, r))
+    for i in comb:
+        if sum(i) == a:
+            combinations.append(i)
+print(combinations)
+print(nums)
+
